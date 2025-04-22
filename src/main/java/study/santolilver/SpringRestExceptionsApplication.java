48b6bac68ecfa7c6b13eb5d135970336397a317f
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-22 15:12:53 -0300

    first commit and publishing the project

---
+++ src/main/java/study/santolilver/SpringRestExceptionsApplication.java
@@ -0,0 +1,13 @@
+package study.santolilver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestExceptionsApplication.class, args);
+	}
+
+}
