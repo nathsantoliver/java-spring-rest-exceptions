bb9e19fc48b77164cc00b6cde312ac5ad7fc4973
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-22 16:05:42 -0300

    managing the HTTP response for exceptions in the controller's action

---
+++ src/main/java/study/santolilver/controllers/PaymentController.java
@@ -0,0 +1,37 @@
+package study.santolilver.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.santolilver.exceptions.NotEnoughMoneyException;
+import study.santolilver.model.ErrorDetails;
+import study.santolilver.model.PaymentDetails;
+import study.santolilver.services.PaymentService;
+
+@RestController
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment")
+    public ResponseEntity<?> makePayment() {
+        try {
+            PaymentDetails paymentDetails = paymentService.processPayment();
+            return ResponseEntity
+                    .status(HttpStatus.ACCEPTED)
+                    .body(paymentDetails);
+        } catch (NotEnoughMoneyException e) {
+            ErrorDetails errorDetails = new ErrorDetails();
+            errorDetails.setMessage("NNot enough money to make the payment.");
+            return ResponseEntity
+                    .badRequest()
+                    .body(errorDetails);
+        }
+    }
+
+}
